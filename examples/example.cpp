e23968140d31b00f7693575af15e49c14175ce90
Yev <evg1985@gmail.com>
2024-09-21 17:09:01 +0200

    example improvements

--- examples/example.cpp
+++ examples/example.cpp
@@ -14,16 +14,16 @@ public:
     
     Person(const std::string& name)
        : Name(name)
-       , Age(0, std::bind(&Person::SetAge, this, std::placeholders::_1))
-       , Address("123 Main St Mega City MS 12345", std::bind(&Person::GetAddress, this))
+       , Age(0, [this](int value) { SetAge(value); })
+       , Address("123 Main St Mega City MS 12345", [this]() { return GetAddress(); })
     {
     }
     
     // For properties with custom getter and setter, we need to define copy constructor
     Person(const Person& other)
         : Name(other.Name())
-        , Age(other.Age(), std::bind(&Person::SetAge, this, std::placeholders::_1))
-        , Address(other.Address.GetRaw(), std::bind(&Person::GetAddress, this)) // Because we declared getter that returns string by value, we need to use GetRaw() to get the initial value
+        , Age(other.Age(), [this](int value) { SetAge(value); })
+        , Address(other.Address.GetRaw(), [this]() { return GetAddress(); }) // Because we declared getter that returns string by value, we need to use GetRaw() to get the initial value
     {
     }
 
@@ -34,7 +34,7 @@ private:
     }
 
     // This getter returns initial address from property and appends predefined country to it
-    std::string GetAddress() {
+    std::string GetAddress() const {
         static std::string predefinedCountry = " USA";
 
         // Don't worry that we call Address() inside getter, it won't cause recursion issue, next call will return the underlying value
@@ -46,7 +46,7 @@ class Office {
 public:
     PropertyRW< std::vector<Person> > Persons;
 
-    void Print() 
+    void Print() const
     {
         for (const Person& p : Persons()) {
             printf("Person: `%s`, `%d`, `%s`\n", p.Name().c_str(), p.Age(), p.Address().c_str());
@@ -58,12 +58,12 @@ public:
 int main()
 {
     Office office;
-    office.Persons().push_back(Person("Alice"));
+    office.Persons().emplace_back(Person("Alice"));
 
     Person john("John");
     // obj.Name = "Alice"; // Won't compile because `Name` is read-only property
     john.Age = 200; // Will be clamped to 150 by setter
-    office.Persons().push_back(john);
+    office.Persons().emplace_back(john);
 
     // Multithreading test, 50/50 chance that Age will be 50 or 100
     Person john2(john); // Copy constructor will be called
@@ -79,7 +79,7 @@ int main()
     t1.join();
     t2.join();
 
-    office.Persons().push_back(john2);
+    office.Persons().emplace_back(john2);
     
     office.Print();
     
